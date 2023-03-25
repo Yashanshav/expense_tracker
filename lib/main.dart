6fcda82f2c24925c7c41991e9371e857bfcca284
yashanshav.purohit <yashanshavvrajpurohit@gmail.com>
2023-03-25 15:38:19 +0530

    Looping Through Lists

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,14 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.purple,
+        fontFamily: 'QuickSand',
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       home: MyHomePage(),
     );
